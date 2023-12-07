2db5b61c5bdf741efe17ccbad33fc1be62280b1b
RATANA Chhorm <chhormratana@gmail.com>
2023-12-07 23:25:30 +0700

    update book get all to datagrid

--- BookShopWinForm/Features/Books/PageBook.Designer.cs
+++ BookShopWinForm/Features/Books/PageBook.Designer.cs
@@ -45,6 +45,7 @@
             btnGetProduct.Size = new Size(87, 32);
             btnGetProduct.TabIndex = 13;
             btnGetProduct.Text = "Get Product";
+            btnGetProduct.Click += btnGetProduct_Click
             // 
             // btnAdd
             // 
--- BookShopWinForm/Features/Books/PageBook.cs
+++ BookShopWinForm/Features/Books/PageBook.cs
@@ -2,6 +2,7 @@
 {
     public partial class PageBook : Form
     {
+        private readonly BookService _bookService;
 
         public PageBook()
         {
@@ -10,6 +11,12 @@
             this.AutoScroll = true;
             this.Dock = DockStyle.Fill;
             this.FormBorderStyle = FormBorderStyle.None;
+
+            // services
+            _bookService = new BookService();
+
+            // default load data
+            loadDate();
         }
         private void btnAdd_Click(object sender, EventArgs e)
         {
@@ -36,6 +43,43 @@
         {
             FormDetail detail = new FormDetail();
             detail.ShowDialog();
+        }
+
+        private void btnGetProduct_Click(object sender, EventArgs e)
+        {
+            loadDate();
+        }
+        private async void loadDate()
+        {
+            var list = await _bookService.GetAllBooks();
+            DataTable table = new DataTable();
+
+            table.Columns.Add("ID", typeof(string));
+            table.Columns.Add("NAME", typeof(string));
+            table.Columns.Add("ISBN", typeof(string));
+            table.Columns.Add("AUTHOR", typeof(decimal));
+            table.Columns.Add("GENRES", typeof(string));
+            table.Columns.Add("PRICE", typeof(decimal));
+
+            foreach (var bk in list.Data)
+            {
+                table.Rows.Add(bk.Id, bk.Name, bk.ISBN, bk.Author, bk.Genres, bk.Price);
+            }
+
+            /**
+         string Id 
+         string ISBN 
+         string Name 
+         decimal Price 
+         string? Description 
+         string Author 
+         string? Genres 
+         DateTime? CreatedOn 
+         DateTime? LastUpdatedOn 
+         DateTime PublishDate 
+            */
+
+            dataGridView1.DataSource = table;
         }
     }
 }
