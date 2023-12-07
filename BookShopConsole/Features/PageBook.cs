fadc61699c7e6a6625d97cc6aadc30b8c855b779
RATANA Chhorm <chhormratana@gmail.com>
2023-12-08 01:08:57 +0700

    update

--- BookShopConsole/Features/PageBook.cs
+++ BookShopConsole/Features/PageBook.cs
@@ -30,7 +30,23 @@ public class PageBook
 
     private static void Viewing()
     {
+        Task.Run(async () =>
+        {
+            BookService bookService = new BookService();
+            Console.WriteLine("\n[Viewing Books]");
+            var result = await bookService.GetAllBooks();
+            var all = result?.Data ?? new();
+            var count = all.Count;
+            Console.WriteLine($"Books: {count}");
+            if (count == 0) return;
 
+            Console.WriteLine($"{"Id",-36} {"ISBN",-20} {"Name",-30} {"Genres",-20} {"Author",-20} {"Price",-10} {"PublishDate",-15} {"Created",-15} {"LastUpdated",-15} ");
+            Console.WriteLine(new string('=', 36 + 1 + 20 + 1 + 30 + 1 + 20 + 1 + 20 + 1 + 10 + 1 + 15 + 1 + 15 + 1 + 15));
+            foreach (var bok in all)
+            {
+                Console.WriteLine($"{bok.Id,-36} {bok.ISBN,-20} {bok.Name,-30} {bok.Genres,-20} {bok.Author,-20} {bok.Price,-10:C} {bok.PublishDate,-15:d} {bok.CreatedOn,-15:d} {bok.LastUpdatedOn,-15:d} ");
+            }
+        }).Wait();
     }
 
     private static void Creating()
